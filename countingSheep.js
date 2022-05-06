cdf26d84c0ef3dbb07222672a76384fd29c6ef29
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-05-05 21:51:51 -0400

    counting sheep

---
+++ countingSheep.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/54edbc7200b811e956000556
+
+function countSheeps(arrayOfSheep) {
+  let counter = 0
+  for(let i = 0; i < arrayOfSheep.length; i++) {
+      if(arrayOfSheep[i]) counter += 1
+  }
+  return counter
+}

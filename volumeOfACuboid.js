2baf2fdd6157c460aa839fdffea4dfc4029b8cd0
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-05-17 23:14:18 -0400

    volume of a cuboid

---
+++ volumeOfACuboid.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    // your code here
+    return length * width * height
+  }
+}

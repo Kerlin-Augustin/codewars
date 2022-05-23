37284067b92de4630630a97915cb12974caa26c6
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-05-22 23:01:14 -0400

    find The Missing Number

---
+++ findTheMissingNumber.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57f5e7bd60d0a0cfd900032d
+
+function missingNo(nums) {
+  
+  for (let i = 0; i <= 100; i++){
+    if(nums.includes(i) === true){
+      
+    } else
+    return i
+  }
+}

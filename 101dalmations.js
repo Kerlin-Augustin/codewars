d9924f05e8016958284ce046ac92d351471d891f
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-06-01 19:51:40 -0400

    101 dalmations

---
+++ 101dalmations.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/56f6919a6b88de18ff000b36
+
+function howManyDalmatians(number){
+  if (number <= 10) {
+    return "Hardly any"
+  } else if (number <= 50) {
+    return "More than a handful!"
+  } else if (number === 101) {
+    return "101 DALMATIANS!!!"
+  } else {
+    return "Woah that's a lot of dogs!" 
+  }
+}

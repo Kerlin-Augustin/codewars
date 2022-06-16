691b00b67fff948f28bebd8dab095488f7da4377
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-06-16 11:35:26 -0400

    exclusive logical operator

---
+++ exclusiveLogicalOperator.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c
+
+// input true or false
+// output true or false
+// a,b both parameters return false
+// a,b if not the same return true
+// if conditional, variable, function, return
+
+
+function xor(a, b) {
+  // TODO: Program Me
+  if(a === b){
+    return false
+  } else {
+    return true
+  }
+}
